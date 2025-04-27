6dfe354df117a43ca4e8c003e258ca6c09805737
lucregrassi <lucre.grassi@gmail.com>
2025-04-27 21:53:27 +0200

    changed path

--- start_microphone.sh
+++ start_microphone.sh
@@ -2,6 +2,6 @@
 export PATH=/usr/bin:/bin:/user/local/bin
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
 sleep 10
-cd /home/pi/cair-audio-processing
-source /home/pi/cair-audio-processing/venv/bin/activate
-python3 /home/pi/cair-audio-processing/audio_recorder.py > /home/pi/audio_recorder_log.txt 2>&1 &
+cd /home/rice/cair-audio-processing
+source /home/rice/cair-audio-processing/venv/bin/activate
+python3 /home/rice/cair-audio-processing/audio_recorder.py > /home/rice/audio_recorder_log.txt 2>&1 &
