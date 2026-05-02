165dee99b36972a748258b8b4ebd096801d7b492
Lucrezia Grassi <lucrezia.grassi@edu.unige.it>
2026-05-02 21:04:05 +0200

    updated path

--- start_microphone.sh
+++ start_microphone.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-export PATH=/usr/bin:/bin:/user/local/bin
+export PATH=/usr/bin:/bin:/usr/local/bin
 export XDG_RUNTIME_DIR=/run/user/$(id -u)
+
 sleep 10
 cd /home/rice/cair-audio-processing
-source /home/rice/cair-audio-processing/venv/bin/activate
-python3 /home/rice/cair-audio-processing/audio_recorder.py > /home/rice/audio_recorder_log.txt 2>&1 &
+
+exec /home/rice/cair-audio-processing/venv/bin/python /home/rice/cair-audio-processing/audio_recorder.py
